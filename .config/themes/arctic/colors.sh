042e9c8591fa71278dd6d0b88949f98d550e2f6e
tanguy42 <tanguy.viv@gmail.com>
2020-09-29 18:27:13 +0200

    Update themes

--- .config/themes/arctic/colors.sh
+++ .config/themes/arctic/colors.sh
@@ -1,3 +1,3 @@
-background="rgba(12,13,16,0.75)"
+background="rgba(12,13,16,0.85)"
 color1="#5e81ac"
 color8="#6C7586"
---
+++ .config/themes/citypop/colors.sh
@@ -0,0 +1,3 @@
+background="rgba(0,0,0,0.85)"
+color1="#69e4e1"
+color8="#87afd7"
